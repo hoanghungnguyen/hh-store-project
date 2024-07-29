2e699d1ff09af771c082e5a0f75dd96559ee05c5
hoanghungnguyen <hoanghunghm2004@gmail.com>
2024-07-29 18:14:39 +0700

    background color header+footer+btn

--- hh-store.vn/admin/layout/footer.php
+++ hh-store.vn/admin/layout/footer.php
@@ -1,9 +1,10 @@
 <div id="footer-wp">
     <div class="wp-inner">
-        <p id="copyright">2018 © Admin Theme by Php Master</p>
+        <p id="copyright">2024 © Admin by HH - Store.vn</p>
     </div>
 </div>
 </div>
 </div>
 </body>
+
 </html>
